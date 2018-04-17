b80884ffe752c9a69a6c0e3c09a1672a5a4fee8f
VitaliiTsybaiev <VitaliiTsybaiev@users.noreply.github.com>
2018-04-17 19:59:45 +0300

    Update App.java

--- src/main/java/selenium/TSV/App.java
+++ src/main/java/selenium/TSV/App.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class App {
 
 
-	public static void   ma in(String[] args) {
+	public static void   m ain (String[] args) {
 
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
